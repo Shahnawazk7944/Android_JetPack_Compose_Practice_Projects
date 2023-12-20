1e2e31a3b99bf8048b6f2513a6d534e69d64d9c3
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2023-12-20 20:08:25 +0530

    GoogleLoginButton design completed commit

--- app/src/main/java/com/example/androidjetpackcomposepracticeprojects/MainActivity.kt
+++ app/src/main/java/com/example/androidjetpackcomposepracticeprojects/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.androidjetpackcomposepracticeprojects
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidjetpackcomposepracticeprojects.ui.theme.AndroidJetPackComposePracticeProjectsTheme
@@ -18,9 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidJetPackComposePracticeProjectsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     GoogleLoginButton()
                 }
@@ -29,18 +32,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidJetPackComposePracticeProjectsTheme {
-        GoogleLoginButton()
+
     }
 }
