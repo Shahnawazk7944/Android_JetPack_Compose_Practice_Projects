c95aa6ae45cc7ee27a3c1368e5feeda7d48bc76d
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-01-02 22:30:54 +0530

    StateManagement in box source code push

--- app/src/main/java/com/example/androidjetpackcomposepracticeprojects/MainActivity.kt
+++ app/src/main/java/com/example/androidjetpackcomposepracticeprojects/MainActivity.kt
@@ -4,20 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.androidjetpackcomposepracticeprojects.data.PersonData
 import com.example.androidjetpackcomposepracticeprojects.ui.theme.AndroidJetPackComposePracticeProjectsTheme
-import com.example.androidjetpackcomposepracticeprojects.ui.theme.gradient_31
+import com.example.androidjetpackcomposepracticeprojects.ui.theme.gradient_21
+import com.example.androidjetpackcomposepracticeprojects.ui.theme.gradient_22
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,50 +28,55 @@ class MainActivity : ComponentActivity() {
             AndroidJetPackComposePracticeProjectsTheme {
                 // A surface container using the 'background' color from the theme
                 Column(
-                    modifier = Modifier.fillMaxSize().background(gradient_31),
+                    modifier = Modifier
+                        .fillMaxSize(),
+//                        .background(gradient_31),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    //Starting Navigation's in JetPack Compose
-                //
-                // will start by tomorrow insA
-                // will start by tomorrow insA
-                // don't know when will i start again
 
-
-
-
-
-
-
-
-
-
-                    //its calling new text filed
-//                    TextFields()
-
-//                    val personData = PersonData()
-//                    val getAllPersonData = personData.getAllPersonsData()
-//                    LazyColumn(
-//                        contentPadding = PaddingValues(all = 12.dp),
-//                        verticalArrangement = Arrangement.spacedBy(12.dp)
-//                    ) {
-//                        items(items = getAllPersonData) { person ->
-//                            PersonDetails(person = person)
-//                        }
-//                    }
-
-
+                    val color = remember {
+                        mutableStateOf(gradient_22)
+                    }
+                    ColorBox(
+                        Modifier.weight(1f).fillMaxSize()
+                    ) {
+                        color.value = it
+                    }
+                    Box(
+                        modifier = Modifier
+                            .background(color.value)
+                            .weight(1f).fillMaxSize()
+                    )
                 }
             }
         }
     }
 }
 
+@Composable
+fun ColorBox(
+    modifier: Modifier = Modifier, updateColor: (Color) -> Unit
+) {
+    Box(
+        modifier = modifier
+            .background(gradient_21)
+            .clickable {
+                updateColor(
+                    Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        1f
+                    )
+                )
+            }
+    )
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    AndroidJetPackComposePracticeProjectsTheme {
-
-    }
+//    ColorBox()
 }
