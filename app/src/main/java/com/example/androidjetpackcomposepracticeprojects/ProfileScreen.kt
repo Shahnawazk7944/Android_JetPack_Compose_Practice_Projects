1437988ee377c52867db51e9920bdf84ff893156
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-01-03 21:10:12 +0530

    Navigation Practice Screens Completed
    Co-Authored-By: Iycs2122 <154696547+Iycs2122@users.noreply.github.com>

---
+++ app/src/main/java/com/example/androidjetpackcomposepracticeprojects/ProfileScreen.kt
@@ -0,0 +1,87 @@
+package com.example.androidjetpackcomposepracticeprojects
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androidjetpackcomposepracticeprojects.ui.theme.ChocolateBrown
+import com.example.androidjetpackcomposepracticeprojects.ui.theme.DarkBrown
+import com.example.androidjetpackcomposepracticeprojects.ui.theme.PaleYellow
+import com.example.androidjetpackcomposepracticeprojects.ui.theme.gradient_31
+import com.example.androidjetpackcomposepracticeprojects.ui.theme.gradient_32
+
+@Composable
+fun ProfileScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(gradient_31),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "PROFILE SCREEN",
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.headlineSmall,
+            color = gradient_32
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                // .height(60.dp)
+                //.width(220.dp)
+                .padding(start = 5.dp, end = 5.dp),
+            colors = ButtonDefaults.buttonColors(DarkBrown),
+            shape = ShapeDefaults.Small
+        ) {
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = null, tint = gradient_32
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = "Back to HOME SCREEN",
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.titleSmall,
+                    color = gradient_32
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen()
+}
