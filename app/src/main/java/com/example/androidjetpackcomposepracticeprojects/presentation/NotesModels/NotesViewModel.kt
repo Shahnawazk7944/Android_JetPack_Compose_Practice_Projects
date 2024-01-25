54b7c2e0a86e7e3c0ad786ac46fbb200d4263256
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-01-25 18:30:43 +0530

    view modal update
    Co-Authored-By: Iycs2122 <154696547+Iycs2122@users.noreply.github.com>

--- app/src/main/java/com/example/androidjetpackcomposepracticeprojects/presentation/NotesModels/NotesViewModel.kt
+++ app/src/main/java/com/example/androidjetpackcomposepracticeprojects/presentation/NotesModels/NotesViewModel.kt
@@ -1,10 +1,40 @@
 package com.example.androidjetpackcomposepracticeprojects.presentation.NotesModels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.androidjetpackcomposepracticeprojects.data.local.NoteDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class NotesViewModel(
     private val dao: NoteDao
 ) : ViewModel() {
+    private val isSortedByDateAdded = MutableStateFlow(true)
+    private var notes = isSortedByDateAdded.flatMapLatest { sort ->
+        if(sort){
+            dao.getNoteOrderdByDateAdded()
+        }else{
+            dao.getNoteOrderByTitle()
+        }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+    fun onEvent(event: NoteEvent) {
+        when (event) {
+            is NoteEvent.DeleteNote -> {
+                viewModelScope.launch {
+                    dao.DeleteNote(event.note)
+                }
 
+            }
+
+            is NoteEvent.SaveNote -> {
+
+            }
+            NoteEvent.SortNotes -> {
+                isSortedByDateAdded.value = ! isSortedByDateAdded.value
+            }
+        }
+    }
 }
