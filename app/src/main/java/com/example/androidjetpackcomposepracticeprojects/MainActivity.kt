ae54c8cb939f45ae0a1372ba85d570084858b16e
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-02-12 12:48:39 +0530

    re-structured
    Co-Authored-By: Iycs2122 <154696547+Iycs2122@users.noreply.github.com>

--- app/src/main/java/com/example/androidjetpackcomposepracticeprojects/MainActivity.kt
+++ app/src/main/java/com/example/androidjetpackcomposepracticeprojects/MainActivity.kt
@@ -4,31 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.androidjetpackcomposepracticeprojects.models.ViewModal
-import com.example.androidjetpackcomposepracticeprojects.ui.SplashScreen
 import com.example.androidjetpackcomposepracticeprojects.ui.theme.AndroidJetPackComposePracticeProjectsTheme
-import com.example.androidjetpackcomposepracticeprojects.ui.theme.gradient_31
-import com.example.androidjetpackcomposepracticeprojects.ui.theme.gradient_32
 
 class MainActivity : ComponentActivity() {
     lateinit var navController: NavHostController
