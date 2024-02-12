6f9ffa91c5bce53898b2c2aeb1db144854656ed1
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-02-12 12:37:05 +0530

    google fonts added
    Co-Authored-By: Iycs2122 <154696547+Iycs2122@users.noreply.github.com>

---
+++ app/src/main/java/com/example/androidjetpackcomposepracticeprojects/ui/theme/Fonts.kt
@@ -0,0 +1,39 @@
+package com.example.androidjetpackcomposepracticeprojects.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.androidjetpackcomposepracticeprojects.R
+
+val playFairFamily = FontFamily(
+    Font(R.font.playfair_display_regular)
+)
+
+val poppins = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_medium, FontWeight.Medium)
+)
+val roboto = FontFamily(
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_thin, FontWeight.Thin)
+)
+val ubuntu = FontFamily(
+    Font(R.font.ubuntu_regular, FontWeight.Normal),
+    Font(R.font.ubuntu_medium, FontWeight.Medium),
+    Font(R.font.ubuntu_bold, FontWeight.Bold),
+    Font(R.font.ubuntu_light, FontWeight.Light),
+    Font(R.font.ubuntu_italic)
+)
+val rubik = FontFamily(
+    Font(R.font.rubik_variable_font_wght, FontWeight.Normal),
+)
+val outfit = FontFamily(
+    Font(R.font.outfit_regular, FontWeight.Normal),
+    Font(R.font.outfit_medium, FontWeight.Medium),
+    Font(R.font.outfit_bold, FontWeight.Bold),
+    Font(R.font.outfit_light, FontWeight.Light),
+    Font(R.font.outfit_thin, FontWeight.Thin)
+)
