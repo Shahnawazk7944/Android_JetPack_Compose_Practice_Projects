78e9d1b0027096fe87cbe8b6dce95b3c0e60792b
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-02-16 21:50:31 +0530

    MVVM practice using fetching Product domain and data layer done
    Co-Authored-By: Iycs2122 <154696547+Iycs2122@users.noreply.github.com>

---
+++ app/src/main/java/com/example/androidjetpackcomposepracticeprojects/store/presentation/product_screen/ProductScreen.kt
@@ -0,0 +1,39 @@
+package com.example.androidjetpackcomposepracticeprojects.store.presentation.product_screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.androidjetpackcomposepracticeprojects.store.presentation.util.components.LoadingDialog
+import com.example.androidjetpackcomposepracticeprojects.store.presentation.util.components.MyTopAppBar
+
+@Composable
+internal fun ProductScreen() {
+
+}
+
+@Composable
+fun ProductContent(
+    state: ProductScreenState
+) {
+    LoadingDialog(isLoading = state.isLoading)
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            MyTopAppBar(title = "Products")
+        }
+    ) {
+        LazyVerticalStaggeredGrid(
+            modifier= Modifier.padding(top = it.calculateTopPadding()),
+            columns = StaggeredGridCells.Fixed(2),
+            ){
+            items(state.product){product ->
+
+            }
+        }
+    }
+}
