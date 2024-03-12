112f4e4ba29521ba05db98b00a50d134250dbe93
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-03-13 00:32:00 +0530

    Margh app task done

--- app/src/main/java/com/example/androidjetpackcomposepracticeprojects/quote/presentation/presentation/product_screen/QuotesProfileScreen.kt
+++ app/src/main/java/com/example/androidjetpackcomposepracticeprojects/quote/presentation/presentation/product_screen/QuotesProfileScreen.kt
@@ -3,15 +3,14 @@ package com.example.androidjetpackcomposepracticeprojects.quote.presentation.pre
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.net.Uri
-import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -20,13 +19,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.DisplayMode.Companion.Input
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -43,17 +42,16 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.drawable.toBitmap
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.size.Size
 import com.example.androidjetpackcomposepracticeprojects.R
-import com.example.androidjetpackcomposepracticeprojects.quote.presentation.presentation.QuotesViewModel
+import com.example.androidjetpackcomposepracticeprojects.quote.presentation.presentation.product_screen.components.ProfileDetailsRowComp
 import com.example.androidjetpackcomposepracticeprojects.quote.presentation.presentation.util.components.QuotesTopAppBar
+import com.example.androidjetpackcomposepracticeprojects.ui.theme.rubik
 import com.example.androidjetpackcomposepracticeprojects.ui.theme.ubuntu
-import kotlinx.coroutines.flow.MutableStateFlow
 
 @SuppressLint("StateFlowValueCalledInComposition")
 @Composable
@@ -68,7 +66,8 @@ fun QuotesProfileScreen(
     }
     val launcher =
         rememberLauncherForActivityResult(
-            contract = ActivityResultContracts.GetContent() ){it ->
+            contract = ActivityResultContracts.GetContent()
+        ) { it ->
             imageUri = it
         }
     bitmap = getImage(uri = imageUri.toString())
@@ -103,7 +102,7 @@ fun QuotesProfileScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
-            Spacer(modifier = Modifier.height(40.dp))
+            Spacer(modifier = Modifier.height(10.dp))
             if (imageUri != null) {
                 bitmap?.let {
                     Image(
@@ -133,6 +132,88 @@ fun QuotesProfileScreen(
                 )
 
             }
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = "Shahnawaz Khan",
+                fontFamily = rubik,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(
+                text = "shahnawazkhan238200@gmail.com",
+                fontFamily = rubik,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+
+            Button(
+                onClick = { },
+                modifier = Modifier
+                    .height(50.dp)
+                    .width(150.dp)
+                    .padding(4.dp),
+                shape = RoundedCornerShape(20.dp),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 2.dp,
+                    pressedElevation = 0.dp
+                ),
+                border = BorderStroke(1.dp, color = Color.LightGray),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFFE403),
+                    //contentColor = SecondaryFontColor
+                )
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Edit Profile",
+                        fontFamily = rubik,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.background
+                    )
+                }
+            }
+            ProfileDetailsRowComp(
+                onClick = {},
+                rowTitle = "Settings",
+                leadingIcon = R.drawable.setting
+            )
+            ProfileDetailsRowComp(
+                onClick = {},
+                rowTitle = "Billing Details",
+                leadingIcon = R.drawable.wallet
+
+            )
+            ProfileDetailsRowComp(
+                onClick = {},
+                rowTitle = "User Management",
+                leadingIcon = R.drawable.terms
+
+            )
+            ProfileDetailsRowComp(
+                onClick = {},
+                rowTitle = "Information",
+                leadingIcon = R.drawable.information
+
+            )
+            ProfileDetailsRowComp(
+                onClick = {},
+                rowTitle = "Logout",
+                rowTitleColor = Color.Red,
+                trailingIconState = false,
+                leadingIcon = R.drawable.logoutt
+
+            )
+
+
         }
     }
 }
