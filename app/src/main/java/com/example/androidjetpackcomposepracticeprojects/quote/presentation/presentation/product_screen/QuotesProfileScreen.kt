207b9e3117c93d010274b4076ab8bfbb6c890842
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-03-12 17:23:13 +0530

    added more element in home page

---
+++ app/src/main/java/com/example/androidjetpackcomposepracticeprojects/quote/presentation/presentation/product_screen/QuotesProfileScreen.kt
@@ -0,0 +1,11 @@
+package com.example.androidjetpackcomposepracticeprojects.quote.presentation.presentation.product_screen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun QuotesProfileScreen(
+    navController: NavHostController
+){
+
+}
