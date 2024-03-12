db07aec9c7cdf8f0fd4b1a2fa1aed16d0551d8dd
Shahnawaz khan <74444644+Shahnawazk7944@users.noreply.github.com>
2024-03-13 01:03:28 +0530

    started splitify task
    Co-Authored-By: Iycs2122 <154696547+Iycs2122@users.noreply.github.com>

--- app/src/main/java/com/example/androidjetpackcomposepracticeprojects/MainActivity.kt
+++ app/src/main/java/com/example/androidjetpackcomposepracticeprojects/MainActivity.kt
@@ -10,25 +10,35 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.androidjetpackcomposepracticeprojects.models.ViewModal
+import com.example.androidjetpackcomposepracticeprojects.splitify.presentation.NavGraph.NavGraph
+import com.example.androidjetpackcomposepracticeprojects.splitify.presentation.NavGraph.Screen
 import com.example.androidjetpackcomposepracticeprojects.store.presentation.product_screen.ProductScreen
 import com.example.androidjetpackcomposepracticeprojects.store.util.Event
 import com.example.androidjetpackcomposepracticeprojects.store.util.EventBus
@@ -40,7 +50,7 @@ class MainActivity : ComponentActivity() {
     lateinit var navController: NavHostController
     private val viewModal by viewModels<ViewModal>()
 
-    @SuppressLint("ShowToast")
+    @SuppressLint("ShowToast", "UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -65,77 +75,50 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    var checkedState by remember { mutableStateOf(false) }
-                    Column (
-                        Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ){
-                        RoundedCornerCheckbox(
-                            label = "Remember Me",
-                            labelSize = 25.sp,
-                            labelColor = Color.Black,
-                            checkedColor = Color(0xFF00CDBD),
-                            uncheckedColor = Color(0xFFf0ffff),
-                            isChecked = checkedState,
-                            size = 40f,
-                            onValueChange = { checkedState = it },
-                            modifier = Modifier.padding(10.dp)
+
+                    val navController = rememberNavController()
+                    var selectedItem by rememberSaveable {
+                        mutableStateOf("home")
+                    }
+                    Scaffold(
+                        bottomBar = {
+                            NavigationBar {
+                                NavigationBarItem(
+                                    selected = selectedItem == "home",
+                                    onClick = {
+                                        selectedItem = "home"
+                                        navController.navigate(Screen.SplitifyHomeScreen.route)
+                                    },
+                                    icon = {
+                                        Icon(
+                                            painter = painterResource(R.drawable.home),
+                                            contentDescription = null,
+                                            modifier = Modifier.size(35.dp).padding(4.dp)
+                                        )
+                                    })
+
+                                NavigationBarItem(
+                                    selected = selectedItem == "history",
+                                    onClick = {
+                                        selectedItem = "history"
+                                        navController.navigate(Screen.SplitifyHistoryScreen.route)
+                                    },
+                                    icon = {
+                                        Icon(
+                                            painter = painterResource(R.drawable.historry),
+                                            contentDescription = null,
+                                            modifier = Modifier.size(35.dp).padding(4.dp)
+                                        )
+                                    })
+
+                            }
+
+                        }
+                    ) {
+                        NavGraph(navController = navController,
                         )
                     }
-
-
-                    //ProductScreen()
                 }
-
-//                Indicator()
-                //SplashScreen()
-//
-//                // A surface container using the 'background' color from the theme
-//                Column(
-//                    modifier = Modifier
-//                        .fillMaxSize()
-//                        .background(gradient_31),
-//                    verticalArrangement = Arrangement.Center,
-//                    horizontalAlignment = Alignment.CenterHorizontally
-//                ) {
-////                    navController = rememberNavController()
-////                    NavGraph(navController = navController)
-//
-//                    var text by remember {
-//                        mutableStateOf("")
-//                    }
-//                    val updatedText = viewModal.newText.collectAsState()
-//                    Text(text = updatedText.value, fontSize = 25.sp, color = gradient_32)
-//                    Spacer(modifier = Modifier.height(30.dp))
-//                    TextField(value = updatedText.value, onValueChange = {
-//                        viewModal.changeText(it)
-//                    })
-
-// will start tomorrow insA
-// will start tomorrow insA
-// will start tomorrow insA
-// will start tomorrow insA
-// will start tomorrow insA
-// will start tomorrow insA
-// will start tomorrow insA
-// will start tomorrow insA
-
-                //its calling new text filed
-//                    TextFields()
-
-//                    val personData = PersonData()
-//                    val getAllPersonData = personData.getAllPersonsData()
-//                    LazyColumn(
-//                        contentPadding = PaddingValues(all = 12.dp),
-//                        verticalArrangement = Arrangement.spacedBy(12.dp)
-//                    ) {
-//                        items(items = getAllPersonData) { person ->
-//                            PersonDetails(person = person)
-//                        }
-//                    }
-
-
             }
         }
     }
